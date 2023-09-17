66ecac17d659908b8f9c687cdef2a49f86fdfbe1
maksimpavlov <maksim.pavlov@Intradiem.com>
2023-09-17 20:38:46 +0200

    Added game engine and samples

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins((DefaultPlugins, HelloPlugin))
+        .run();
+}
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app
+            .insert_resource(GreetTimer(
+                Timer::from_seconds(2.0, TimerMode::Repeating))
+            )
+            .add_systems(Startup, add_people)
+            .add_systems(Update, (hello_world, greet_people));
+    }
+}
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
+fn hello_world() {
+    println!("Hello World!");
+}
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Renzo Hume".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+}
+
+fn greet_people(
+    time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("Hello {}!", name.0);
+        }
+    }
 }
